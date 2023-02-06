47f49bce2e3de1a5e7df7b9ca23ea0d730a9149a
Azmir <azmir02alif@gmail.com>
2023-02-06 19:05:51 +0600

    api another url

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.get("/", function (req, res) {
   ]);
 });
 
+app.get("/test", function (req, res) {
+  res.json({
+    message: "Successful Try",
+  });
+});
+
 app.listen(3000, function () {
   console.log(chalk.bgBlue("Port Running On 3000"));
 });
