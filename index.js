9849fa0f91e756f048c35076ba31c0192bc8bdcd
Azmir <azmir02alif@gmail.com>
2023-02-08 19:48:17 +0600

    user model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const chalk = require("chalk");
 const cors = require("cors");
 const routes = require("./routes");
+const setupDB = require("./config/dbConfig");
 const app = express();
 
 console.log(process.env.BASE_URL);
@@ -11,7 +12,7 @@ console.log(process.env.BASE_URL);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
+setupDB();
 app.use(routes);
 
 app.listen(3000, function () {
---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  merchant: {
+    type: Schema.Types.ObjectId,
+    ref: "Merchant",
+    default: null,
+  },
+  googleId: {
+    type: String,
+  },
+  facebookId: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "member",
+    enum: ["admin", "member", "merchant"],
+  },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: Date,
+  },
+  updated: {
+    type: Date,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
