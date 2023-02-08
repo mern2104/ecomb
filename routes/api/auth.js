b9c208a0e34c40ca6cddde1781392ac5454acb19
Azmir <azmir02alif@gmail.com>
2023-02-08 19:17:57 +0600

    auth api validation  check for registration

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,9 +3,20 @@ const _ = express.Router();
 
 // registration
 _.post("/registration", (req, res) => {
-  let { email, name } = req.body;
+  let { email, fullname, password, isSubscribe } = req.body;
 
-  res.json({ email, name });
+  if (!email) {
+    return res.json({ error: "You must enter an email address" });
+  }
+
+  if (!fullname) {
+    return res.json({ error: "You must enter an fullname" });
+  }
+  if (!password) {
+    return res.json({ error: "You must enter an password" });
+  }
+
+  res.json({ email, fullname, password, isSubscribe });
 });
 
 module.exports = _;
