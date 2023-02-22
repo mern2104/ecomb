a762220311fb5bae77df02a31704793918eef17c
Shohelrana63 <shohelrana.beg@gmail.com>
2023-02-22 18:54:25 +0600

    email send

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,9 +4,8 @@ const User = require("../../models/user.js");
 var jwt = require("jsonwebtoken");
 const { sendVerificationEmail } = require("../../utils/emailSender.js");
 
-
 // registration
-_.post("/registration", (req, res) => {
+_.post("/registration", async (req, res) => {
   let { email, phoneNumber, firstName, lastName, password } = req.body;
 
   if (!email) {
@@ -25,6 +24,12 @@ _.post("/registration", (req, res) => {
     return res.json({ error: "You must enter an password" });
   }
 
+  let duplicateEmail = await User.find({ email: email });
+
+  if (duplicateEmail) {
+    return res.json({ error: "Already email exists" });
+  }
+
   const user = new User({
     email,
     phoneNumber,
@@ -35,13 +40,13 @@ _.post("/registration", (req, res) => {
 
   user.save();
 
-  let username = user.firstName+user.lastName
+  let username = user.firstName + user.lastName;
 
   let token = jwt.sign({ email: user.email }, ">@H%rH@>MDW(N72", {
     expiresIn: "1h",
   });
 
-  sendVerificationEmail(user.email,username,token)
+  sendVerificationEmail(user.email, username, token);
 
   res.json(user);
 });
